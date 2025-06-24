807976243be1aa4e7105e4043c5afe32af3f3870
yassir ali <aliyassir859@gmail.com>
2025-06-24 16:20:31 +0200

    feat: add routing and create initial page components for Home, About, Category, Boats, Details, and Contact

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,27 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Client/pages/Home.jsx';
+import About from './Client/pages/About.jsx';
+import Category from './Client/pages/Category.jsx';
+import Boats from './Client/pages/Boats.jsx';
+import Details from './Client/pages/Details.jsx';
+import Contact from './Client/pages/Contact.jsx';
 
 function App() {
 
   return (
     <Router>
       <Routes>
+        {/* Clients page */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/boats" element={<Boats />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/contact" element={<Contact />} />
 
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
