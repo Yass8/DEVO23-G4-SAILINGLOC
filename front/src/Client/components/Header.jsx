da6a8509dd1ac9f6eec4074042731fb246c01a39
Leslie OUINSOU <ouinsoul5@gmail.com>
2025-06-26 18:15:55 +0200

    Add banner section with image and text

--- front/src/Client/components/Header.jsx
+++ front/src/Client/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from "/images/logo.png";
-import banner from "/images/hero.jpeg"; 
+ 
 
 const Header = () => {
   return (
@@ -44,24 +44,6 @@ const Header = () => {
 
         </div>
       </div>
-<div className="relative w-full h-[180px] sm:h-[220px] md:h-[260px] lg:h-[300px]">
-  <img
-    src={banner}
-    alt="Bannière"
-    className="w-full h-full object-cover"
-  />
-  <div className="absolute top-1/2 left-4 sm:left-10 -translate-y-1/2 text-black space-y-2">
-    <h2 className="text-lg sm:text-xl md:text-2xl font-bold leading-snug">
-      Trouvez le bateau parfait<br />
-      pour votre prochaine<br />
-      aventure
-    </h2>
-    <button className="mt-1 bg-[#c58e6a] text-white px-3 sm:px-5 py-1.5 sm:py-2 rounded-md text-sm sm:text-base font-medium">
-      Commencez votre aventure
-    </button>
-  </div>
-</div>
-
 </header>
   );
 };
--- front/src/Client/pages/Details.jsx
+++ front/src/Client/pages/Details.jsx
@@ -1,13 +1,14 @@
-
-
+import Banner from "../components/Banner";
 function Details() {
-  return (
+  return ( 
+    <>
+    <Banner/>
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Details Page</h1>
       <p className="text-lg">This is the details page.</p>
     </div>
+    </>
   );
+  
 }
-
-
 export default Details;
