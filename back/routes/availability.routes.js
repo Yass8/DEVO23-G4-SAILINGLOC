7859b36633fa0a0e05d53513bba9d05960f6e8a6
yassir ali <aliyassir859@gmail.com>
2025-07-11 15:15:29 +0200

    update availability.routes.js

--- back/routes/availability.routes.js
+++ back/routes/availability.routes.js
@@ -13,11 +13,11 @@ const router = express.Router();
 
 router.get("/", availabilityController.index);
 router.post("/new", validateCreateAvailability, validate, availabilityController.create);
-router.get("/:id/show", availabilityController.show);
-router.put("/:id/edit", availabilityController.update);
-router.delete("/:id/delete", availabilityController.remove);
+router.get("/:id/show", validateAvailabilityId, validate, validateAvailabilityId, validate, availabilityController.show);
+router.put("/:id/edit", validateUpdateAvailability, validate, availabilityController.update);
+router.delete("/:id/delete", validateAvailabilityId, validate, availabilityController.remove);
 
 // Route spécifique
-router.get("/boat/:boatId", availabilityController.getBoatAvailabilities);
+router.get("/boat/:boatId", validateBoatId, validate, availabilityController.getBoatAvailabilities);
 
 export default router;
