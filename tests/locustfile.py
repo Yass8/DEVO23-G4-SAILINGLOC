8ae76c23ccc33dc3eccfeaac37e5d356f34df69b
MEMS17 <eddymensah243@gmail.com>
2025-07-16 10:36:18 +0200

    feat: update devcontainer configuration and add locust service to docker-compose

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -4,3 +4,8 @@ class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/api/v1")
+        self.client.get("/api/v1/users")
+        self.client.get("/api/v1/boats")
+        self.client.get("/api/v1/reservations")
+        self.client.get("/api/v1/ports")
+        self.client.get("/api/v1/users-documents")
