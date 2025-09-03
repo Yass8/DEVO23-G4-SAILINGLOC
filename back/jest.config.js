2d8c3eb26ea0a5d7d6eeb94b3ca9727c0422e034
yassir ali <aliyassir859@gmail.com>
2025-09-03 17:57:18 +0200

    feat: Add Jest configuration and unit tests for user controller and service

---
+++ back/jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'controllers/**/*.js',
+    'services/**/*.js',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: './coverage',
+  testMatch: ['**/tests/unit/**/*.test.js'],
+  transform: {},
+  testTimeout: 30000,
+  moduleNameMapper: {  
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  clearMocks: true,
+  resetMocks: true
+};
